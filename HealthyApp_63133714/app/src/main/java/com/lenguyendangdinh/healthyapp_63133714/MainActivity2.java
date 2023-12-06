fc106ae325d375c2fe9d614f381e44bf3e8f5c9f
Djnk2203 <131732391+Djnk2203@users.noreply.github.com>
2023-12-06 16:12:41 +0700

    up
    up

---
+++ HealthyApp_63133714/app/src/main/java/com/lenguyendangdinh/healthyapp_63133714/MainActivity2.java
@@ -0,0 +1,14 @@
+package com.lenguyendangdinh.healthyapp_63133714;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
