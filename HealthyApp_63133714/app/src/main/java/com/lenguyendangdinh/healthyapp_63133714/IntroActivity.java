488f409f3e14bef7cba66b0089581985bd0b9fd4
Djnk2203 <131732391+Djnk2203@users.noreply.github.com>
2023-12-13 00:57:44 +0700

    up

---
+++ HealthyApp_63133714/app/src/main/java/com/lenguyendangdinh/healthyapp_63133714/IntroActivity.java
@@ -0,0 +1,21 @@
+package com.lenguyendangdinh.healthyapp_63133714;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class IntroActivity extends AppCompatActivity {
+//    Button btnGoHome;
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        btnGoHome = findViewById(R.id.introBtn);
+//        btnGoHome.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+//                startActivity(intent);
+//            }
+//        });
+//    }
+}
